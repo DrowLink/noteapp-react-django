d784a7309c8d69c103486785acb6c0415f566f42
drowlink <pauloriveiro01@gmail.com>
2023-08-26 18:02:44 -0400

    saving

--- frontend/src/pages/NoteListPage.js
+++ frontend/src/pages/NoteListPage.js
@@ -10,7 +10,7 @@ export const NoteListPage = () => {
 
 
     let getNotes = async () => {
-        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let response = await fetch('/api/notes/')
         let data = await response.json()
         console.log('DATA: ', data)
         setNotes(data)
