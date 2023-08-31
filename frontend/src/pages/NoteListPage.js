ffd7f0a2966412a721ceb9a3343108fa5ce45bcf
drowlink <pauloriveiro01@gmail.com>
2023-08-31 16:04:04 -0400

    finally cors resolved

--- frontend/src/pages/NoteListPage.js
+++ frontend/src/pages/NoteListPage.js
@@ -2,19 +2,20 @@ import React, { useState, useEffect } from 'react'
 
 export const NoteListPage = () => {
 
-    let [ notes, setNotes ] = useState([])
+    let [notes, setNotes] = useState([])
 
-    useEffect(() => {
-        getNotes()
-    }, [])
+     useEffect(() => {
+         getNotes()
+     }, [])
 
 
-    let getNotes = async () => {
-        let response = await fetch('/api/notes/')
-        let data = await response.json()
-        console.log('DATA: ', data)
-        setNotes(data)
-    }
+     let getNotes = async () => {
+         let response = await fetch('http://127.0.0.1:8000/api/notes/')
+         let data = await response.json()
+         setNotes(data)
+         console.log(`DATA : ${notes}`)
+         
+     }
 
 
   return (
@@ -22,6 +23,7 @@ export const NoteListPage = () => {
         { notes.map((note, index) => (
            <h3 key={index}>{note.body}</h3>
         ))}
+        Hello!
     </div>
   )
 }
