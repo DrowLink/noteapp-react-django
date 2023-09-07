8953713188436e90c59303e3f74b01923e0f19f3
drowlink <pauloriveiro01@gmail.com>
2023-09-07 13:50:13 -0400

    rest api done

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -1,8 +1,11 @@
+from django.http import response
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
 from .models import Note
 from .serializers import NoteSerializer
+from api import serializers
 from .utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
 
 # Create your views here.
@@ -57,39 +60,12 @@ def getNotes(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def getNote(request, pk):
 
-    if request.response == 'GET':
+    if request.method == 'GET':
         return getNoteDetail(request, pk)
 
-    if request.response == 'PUT':
+    if request.method == 'PUT':
             return updateNote(request, pk)
-            # data = request.data
-            # note = Note.objects.get(id=pk)
-            # serializer = NoteSerializer(instance=note, data=data)
 
-            # if serializer.is_valid(): 
-            #     serializer.save()
-
-            # return Response(serializer.data)
-
-    if request.response == 'DELETE':
+    if request.method == 'DELETE':
         return deleteNote(request, pk)
     
-
-# @api_view(['PUT'])
-# def updateNote(request, pk):
-#     data = request.data
-#     note = Note.objects.get(id=pk)
-#     serializer = NoteSerializer(instance=note, data=data)
-
-#     if serializer.is_valid(): 
-#         serializer.save()
-
-#     return Response(serializer.data)
-
-
-
-# @api_view(['DELETE'])
-# def deleteNote(request, pk):
-#     note = Note.objects.get(id=pk)
-#     note.delete()
-#     return Response('Note was deleted')
